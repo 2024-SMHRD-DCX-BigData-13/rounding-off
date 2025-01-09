2e119bd19f5ad3340f5f805bce955c282adaec30
yonghyun <dydgus0803@gmail.com>
2025-01-09 17:47:23 +0900

    커밋밋

--- webServer/static/js/main.js
+++ webServer/static/js/main.js
@@ -1,13 +1,11 @@
+document.getElementById('loginButton').addEventListener('click', () => {
+  window.location.href = '/login'; // 로그인 페이지 URL
+});
 
-  document.getElementById('loginButton').addEventListener('click', () => {
-    window.location.href = '/login'; // 로그인 페이지 URL
-  });
+// 화면 다크모드(상시)
+document.querySelector('.text-bg-primary').style.backgroundColor = '#FF69B4';
 
-  // 화면 다크모드(상시)
-  document.querySelector('.text-bg-primary').style.backgroundColor = '#FF69B4';
-
-  
-  // 로그인시 관심종목과  자동매매 보여주는 함수
+// 로그인시 관심종목과  자동매매 보여주는 함수
 // document.addEventListener('DOMContentLoaded', () => {
 //   // 서버에서 로그인 상태 확인
 //   fetch('/api/check-login')
@@ -39,8 +37,8 @@
 document.addEventListener('DOMContentLoaded', () => {
   // 서버에서 로그인 상태 확인
   fetch('/api/check-login')
-    .then(response => response.json())
-    .then(data => {
+    .then((response) => response.json())
+    .then((data) => {
       if (data.isLoggedIn) {
         // 로그인 상태일 때 관심종목과 자동매매 섹션 표시
         document.getElementById('favorites').style.display = 'block';
@@ -64,7 +62,6 @@ document.addEventListener('DOMContentLoaded', () => {
         mypageButton.addEventListener('click', () => {
           window.location.href = '/mypage'; // 로그인 페이지 URL
         });
-        
 
         // 새로 생성된 로그아웃 버튼
         const logoutButton = document.createElement('button');
@@ -81,13 +78,9 @@ document.addEventListener('DOMContentLoaded', () => {
             .then(() => {
               window.location.href = '/main';
             })
-            .catch(error => console.error('Error:', error));
+            .catch((error) => console.error('Error:', error));
         });
-        
-
-        
-
       }
     })
-    .catch(error => console.error('Error:', error));
+    .catch((error) => console.error('Error:', error));
 });
