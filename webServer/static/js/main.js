85513ede1245c902e9e8d9580361ec827ab1e874
yonghyun <dydgus0803@gmail.com>
2025-01-15 10:20:23 +0900

    1--15

--- webServer/static/js/main.js
+++ webServer/static/js/main.js
@@ -14,6 +14,20 @@ document.getElementById('logoutButton').addEventListener('click', () => {
     .catch((error) => console.error('Error:', error));
 });
 
+// 로그인 상태 확인
+const recommendSection = document.getElementById('recommend-section');
+
+// 로그인 함수
+function login() {
+  recommendSection.classList.remove('not-logged-in');
+  recommendSection.classList.add('logged-in');
+}
+
+// 로그아웃 함수
+function logout() {
+  recommendSection.classList.remove('logged-in');
+  recommendSection.classList.add('not-logged-in');
+}
 // 화면 다크모드(상시)
 document.querySelector('.text-bg-primary').style.backgroundColor = '#FF69B4';
 
@@ -23,14 +37,13 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((response) => response.json())
     .then((data) => {
       if (data.isLoggedIn) {
-        // 로그인 상태일 때 관심종목과 자동매매 섹션 표시
+        // 로그인 상태일 때 관심종목 섹션 표시
         document.getElementById('favorites').style.display = 'block';
-        document.getElementById('auto-trading').style.display = 'block';
         // document.getElementsByClassName('intro').style.display = 'none';
         document.getElementById('loginButton').style.display = 'none';
         document.getElementById('logoutButton').style.display = 'block';
         document.getElementById('mypageButton').style.display = 'block';
-        document.getElementById('image-content').style.display = "none";
+        document.getElementById('image-content').style.display = 'none';
         document.getElementById('opentext').style.display = 'none';
       }
     })
